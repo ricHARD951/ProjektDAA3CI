ac262ce6a34483a48583cdaa3c2e5787c8637cd8
z19istc08 <z19istc08@z00p3-05.SPSKNM>
2022-02-23 11:37:53 +0100

    Update Connection.php

--- php_scripts/Connection.php
+++ php_scripts/Connection.php
@@ -13,5 +13,19 @@ if (!$conn) {
 }
 //echo "Connected successfully </br>";
 
+$sql = "SELECT id, username, password FROM Users";
+$result = mysqli_query($conn, $sql);
+print_r(mysqli_num_rows($result));
+
+
+if(mysqli_num_rows($result) > 0){
+while($row = mysqli_fetch_assoc($result)){
+  echo "id: " . $row["id"] ."- Name: " . $row["username"] . " " . "<br>";
+}
+}else{
+echo "0  results";
+}
+
+
 
 ?>
