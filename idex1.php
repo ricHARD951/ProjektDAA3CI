c1f3aff98be9f1788233b7106b541300b97706fa
z19istc08 <z19istc08@z00p3-05.SPSKNM>
2022-03-23 11:44:45 +0100

    bootstrap buttonik

--- idex1.php
+++ idex1.php
@@ -8,12 +8,15 @@ require 'php_scripts/Connection.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/app.css">
-    <title>Document</title>
+    <title>Main page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <link rel= "stylesheet" href="./style/style_register.css"> 
 <body> 
   <a href="./php_scripts/Login.php">Login</a>
   <a href="./php_scripts/Register.php">Register</a>
+  <button type="button" class="btn btn-primary">Nemačkať</button>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
 
